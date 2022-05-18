92500db1f4ab9abd9dfc240678b6a2b3146a1751
Nuria <nuria.godino@babelgroup.com>
2022-05-18 13:20:39 +0200

    ejercicio finalizado

---
+++ src/main/java/init/GAtewaySupermercadoApplication.java
@@ -0,0 +1,13 @@
+package init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GAtewaySupermercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GAtewaySupermercadoApplication.class, args);
+	}
+
+}
